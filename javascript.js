ab3c24942e259b5c8cfb9f126ac10938cb57ffff
bekahlundy <lundyrebekah@gmail.com>
2016-10-19 13:06:20 -0600

    add read button, works.

--- javascript.js
+++ javascript.js
@@ -1,28 +1,35 @@
 
 
-
-
-
-
-
-
 $('.enter').click(function(){
+  createCards();
+});
+
+
+
+function createCards() {
   var title = $('.title').val();
   var url = $('.url').val();
-  $('.right').append(`<li class ='box'><div class='title-result'>${title}</div>
-                        <div class='url-result'>${url}</div>
-                        <button class='read-button' type='button'>read</button>
-                        <button class='delete'>delete</button></li>`)
+
+  $('.right').append(`<li class ='box'>
+
+        <div class='title-result'>${title}
+        </div>
+
+        <div class='url-result'>${url}
+        </div>
+
+        <button class='read-button' type='button'>read
+        </button>
+
+        <button class='delete'>delete
+        </button>
+  </li>`)
   console.log($('.read-button'))
+
+};
+
+
+
+$('.right').click('.read-button', function() {
+   $('.box').toggleClass('read');
 });
-
-
-
-$('.read-button').click(function() {
-   ;
-  $('.box').toggleClass('read');
-});
-
-
-
-// $(.'')
