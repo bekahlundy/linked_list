f5bb1826e3430f8dd0d1a92e8e638c28a8733c7c
Noah Peden <noahpeden@gmail.com>
2016-10-19 12:23:11 -0600

    work on delete and read buttons

--- javascript.js
+++ javascript.js
@@ -9,10 +9,20 @@
 $('.enter').click(function(){
   var title = $('.title').val();
   var url = $('.url').val();
-$('.right').append(`<li class ='box'><div class='title-result'>${title}</div>
-                      <div class='url-result'>${url}</div>
-                      <button class='read'>read</button>
-                      <button class='delete'>delete</button></li>`)
+  $('.right').append(`<li class ='box'><div class='title-result'>${title}</div>
+                        <div class='url-result'>${url}</div>
+                        <button class='read-button' type='button'>read</button>
+                        <button class='delete'>delete</button></li>`)
+  console.log($('.read-button'))
 });
+
+
+
+$('.read-button').click(function() {
+   ;
+  $('.box').toggleClass('read');
+});
+
+
 
 // $(.'')
