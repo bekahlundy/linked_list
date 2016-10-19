97185735b99b345f7e17b0dfc18bc8024d619b1d
bekahlundy <lundyrebekah@gmail.com>
2016-10-18 21:31:17 -0600

    add most css. still some to go. add logo svg

--- javascript.js
+++ javascript.js
@@ -9,10 +9,10 @@
 $('.enter').click(function(){
   var title = $('.title').val();
   var url = $('.url').val();
-$('.right').append(`<li><div class='title'>${title}</div>
-                      <div class='url'>${url}</div>
+$('.right').append(`<li class ='box'><div class='title-result'>${title}</div>
+                      <div class='url-result'>${url}</div>
                       <button class='read'>read</button>
                       <button class='delete'>delete</button></li>`)
 });
 
-$(.'')
+// $(.'')
